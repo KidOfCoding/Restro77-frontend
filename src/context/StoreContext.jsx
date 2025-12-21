ff450b88d51e9758ff3ec6be96f54f1edfb93e59
jaga07u <pradhan02jagadish@gmail.com>
2025-12-21 16:18:28 +0530

    server url attached

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const StoreContextProvider = (props) => {
   const [cartItem, setCartItems] = useState({});
-  const URl = "http://localhost:4000"
+  const URl = "https://restro77-backend.vercel.app/"
   const [token , setToken] = useState("")
   const [food_list,setFoodList] = useState([])
 
